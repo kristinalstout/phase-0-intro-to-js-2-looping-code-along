883586c52998b6ae45623889839d4c4076b30060
Kristina Stout <kristinalstout@gmail.com>
2023-05-31 17:36:26 -0400

    created for and while loops in functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+let name = [];
+let event;
+function writeCards(name,event){
+    let thankYouCards = [];
+    for (let i = 0; i < name.length; i++){
+      thankYouCards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+   return thankYouCards;
+}
+writeCards(["Guadalupe","Ollie","Aki"],"surprise");
+
+let number;
+function countDown(number){
+    while (number>=0){
+        console.log(number--);
+    }
+}
